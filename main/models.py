bd195cf6b32cfaed74a35051dd4c11c1af27c1d0
Rafael <rafa.brito295@gmail.com>
2026-07-04 16:37:08 -0300

    model, database: modify atributes model and add table client in the table

--- main/models.py
+++ main/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Cliente(models.Model):
+    # user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    nome_cliente = models.CharField(max_length= 255,null=True)
+    morada = models.CharField(max_length = 255, null=True)
+    cidade = models.CharField(max_length = 50, null=True)
+    telefone = models.CharField(max_length = 50, null=True)
+    purl = models.CharField(max_length = 255, blank=True, null=True)
+    ativo = models.BinaryField(False)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    updated_at = models.DateTimeField(auto_now_add=True, null=True)
+    deleted_at = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self) -> str:
+        return self.nome_cliente
